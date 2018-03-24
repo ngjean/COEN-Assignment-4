62c3ae71696fc89df96ca5ee76987a4d0c83242f
gabrielarturo94 <36458153+gabrielarturo94@users.noreply.github.com>
2018-03-23 23:38:16 -0400

    Update Vertex.h

--- Polymorphism&OperationOverloading/include/Vertex.h
+++ Polymorphism&OperationOverloading/include/Vertex.h
@@ -1,23 +1,35 @@
 #ifndef VERTEX_H
 #define VERTEX_H
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include "Edge.h"
 using namespace std;
+
 class Vertex
 {
     public:
         Vertex();
         Vertex(int,int);
         virtual ~Vertex();
-        Vertex(const Vertex& other);
-        int getValue()const;
-        int getId()const;
-       // int getIconnect()const;
-       // void setIconnet(int);
+        Vertex(const Vertex&);
+
         void setValue(int);
+        int getValue()const;
+        void setId(int);
+        int getId()const;
+
+        //void setConnection(const Edge&);
+        //void removeConnection(const Edge&);            // For what I was trying to do with the Vector of Connections
+        int getIconnect()const;
+
+        void setNext(Vertex);		// To set the Vertex to which this one is going to point to.
+        							// (I don't know how to properly pass the object Vertex in this funct.)
+
         //void setConnection(Edge& );
 
     private:
-        //vector<Edge> Connection;
-        int id, value;//,iconnect;
+        //vector<Edge> connections;
+        int id, value, iconnect;
+        //Vertex* next;     I don't know where are we going to use it now
+        bool visited;
 };
-#endif // VERTEX_H
