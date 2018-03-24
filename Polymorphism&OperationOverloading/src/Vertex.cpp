7f91a9b8b839a84c67955accee1d70b58a5b1c99
gabrielarturo94 <36458153+gabrielarturo94@users.noreply.github.com>
2018-03-23 23:32:26 -0400

    Update Vertex.cpp
    Don't know if with your approach we are going to use Vertex *next

--- Polymorphism&OperationOverloading/src/Vertex.cpp
+++ Polymorphism&OperationOverloading/src/Vertex.cpp
@@ -7,43 +7,64 @@ Vertex::Vertex()
 {
     value = 0;
     id = 0;
-    //iconnect = 0;
+    iconnect = 0;
+    //next = new Vertex();
+    //next = NULL;
+    visited = false;
 }
 
 Vertex::Vertex(int iid,int ivalue)
 {
    id = iid;
    value = ivalue;
-  // iconnect = 0;
+   iconnect = 0;
+   //next = new Vertex();
+   //next = ptr;
+   visited = false;
 }
 
 Vertex::~Vertex()
 {
-    //deleted vectex
+    //delete vertex
 }
 
 Vertex::Vertex(const Vertex& other)
 {
-
+    value = other.getValue();
+    id = other.getId();
+    //next = other.next;
+    visited = other.visited;
+    iconnect = other.iconnect;
 }
 
-/*int Vertex::getIconnect()const
+int Vertex::getIconnect()const
 {
     return iconnect;
 }
 
-void Vertex::setIconnet(int inum)
+/*void Vertex::setIconnet(int inum)
 {
+}*/
 
-}
-
-void Vertex::setConnection(Edge& newConnection)
+void Vertex::setConnection(const Edge& newConnection)
 {
-    Edge next = newConnection;
-    Connection.push_back(next);
+    // Edge next = newConnection;				I don't think this is necessary
+    connections.push_back(newConnection);
     iconnect++;
 }
-*/
+
+void Vertex::removeConnection(const Edge& remo){
+
+	for (int j ; j <= connections.size() ; j++ ){
+
+		if (connections[j].dest == remo.dest || connections[j].start == remo.start ){
+
+			connections.erase(connections.begin() - (j-1));
+		}
+	}
+
+}
+
 
 int Vertex::getId()const
 {
@@ -58,4 +79,14 @@ int Vertex::getValue()const
 void Vertex::setValue(int ivalue)
 {
     value = ivalue;
+}
+
+void Vertex::setId(int iid){
+
+	id = iid;
+}
+
+void Vertex::setNext(Vertex v){
+
+	next = &v;
 }
